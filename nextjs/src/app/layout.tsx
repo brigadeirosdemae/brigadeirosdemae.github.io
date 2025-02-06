da3c94a0c90ab0445a537de5c75adeb9170cfb24
Gabriel Mendonça <gabriel.mendonca_ext@olos.com.br>
2025-02-06 04:08:37 +0000

    Add first site

---
+++ nextjs/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Brigadeiros de Mãe - Doces Gourmet",
+  description: "O Melhor do doce em Taboão da Serra e Região",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt_BR">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
